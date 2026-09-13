0bef999711579988623161da5292ce4027fecb88
ajinkya <ajinkya@github.com>
2026-09-13 10:26:30 +0530

    motivation is cheap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+  return (
+	  <>
+	  <MovieCard movie={{title:"Ajinkya",release_date:'2023'}}/>
+	  <MovieCard movie={{title:"Ajinkya",release_date:'2023'}}/>
+	  </>
+  )
+}
+
+export default App
